c4bfb27eefa420686d501116ef2c75325927eb05
D-Brack <derrek.brack@gmail.com>
2023-05-02 11:10:08 -0500

    8. Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config;
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+const privateKeys = process.env.SEPOLIA_DAPPU_DEV_PRIVATE_KEY || ''
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18", /**video tutorial is set at 0.8.9 */
+  solidity: '0.8.18', /**video tutorial is set at 0.8.9 */
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   },
+
 };
